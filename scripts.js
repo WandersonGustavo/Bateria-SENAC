9737cb37376a95c43da5a104ee8a3c126a1256f1
WandersonGustavo <pqnohobbitateu@gmail.com>
2023-11-29 01:19:34 -0300

    correção no js

--- scripts.js
+++ scripts.js
@@ -27,6 +27,51 @@ function playSound(sound){
     }
 }
 
+function playComposition(songArray) {
+    let wait = 0;
+
+    for(let songItem of songArray) {
+        setTimeout(()=>{
+            playSound(`key${songItem}`);
+        }, wait);
+
+        wait += 500;
+    }
+}
+
+function playComposition(songArray) {
+    let wait = 0;
+
+    for(let songItem of songArray) {
+        setTimeout(()=>{
+            playSound(`key${songItem}`);
+        }, wait);
+
+        wait += 500;
+    }
+}
+
+const volumeControl = document.querySelector('#volume-range');
+
+volumeControl.addEventListener('input', (evento) => {
+    setVolume(evento.target.value);
+});
+
+function setVolume(volume){
+    const audioElements = document.querySelectorAll('audio');
+    audioElements.forEach((audio) => {
+        audio.volume = volume;
+    });
+}
+
+
+
+
+
+
+
+
+
 
 
 
